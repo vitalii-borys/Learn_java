a59cbb864eb99c65033825e8823a90575bbafa41
vitalii-borys <photoshopprofy@gmail.com>
2024-03-07 02:55:47 -0600

    notebook inverted 2Darray added

--- twoDarrayrotate.java
+++ twoDarrayrotate.java
@@ -1,12 +1,13 @@
 public class twoDarrayrotate {
     public static void main(String[] args) {
         int[][] myArray = {
-            {1,2,3},
-            {4,5,6}
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12}
         };
-        for (int a=0; a<myArray.length; a++) {
-            for (int b=0; b<myArray[a].length; b++) {
-                System.out.print(myArray[a][b]);
+        for (int a=0; a<myArray[0].length; a++) {
+            for (int b=0; b<myArray.length; b++) {
+                System.out.print(myArray[b][a] + " ");
             }
             System.out.println();
         }
