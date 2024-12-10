8156e00974bae0d2711bf9a678b65db02593e06e
photoshopprofy <photoshopprofy@gmai.com>
2024-12-09 23:07:00 -0600

    Game запише файл

--- Game.java
+++ Game.java
@@ -12,7 +12,7 @@ public class Game {
                 for (int i = 0; i < n.length(); i++) {
                     fw.write(n.charAt(i));
                 }
-                System.out.println("Файл успішно записаний");
+                System.out.println("Файл \"Відучити від java.txt\" успішно записаний");
                 fw.close(); 
             } 
             catch (Exception e) { 
@@ -31,11 +31,27 @@ public class Game {
         }
     }
     public static void main(String[] args) {
-        System.out.println("Привіт");
+        System.out.println("О, хтось ще грає текстові ігри в 2024 році? Як тебе звати?");
 		Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println(name + ", привіт. Ти у звичайній грі, де треба знайти вихід з гри.");
+        System.out.println("Тут ти можеш іти \"і\" в напрямку сторін горизонту або спробувати взаємодіяти \"в\" з предметами.\"");
+        System.out.println("---Приклад---\"і північ\"---\"в стіл\"---");
+        System.out.println("...");
+        System.out.println("Ти просинаєшся на підлозі в пустій кімнаті одноповерхового будинку.");
+        System.out.println("Прямо двері, справа двері. Справа стіна з картиною.");
+        System.out.println("Ззаду вікно, в якому видно схід сонця. Але на ньому решітка.");
+        System.out.println("Що робимо?");
+        System.out.println("...");
         String input = scan.nextLine();
+        if (input.equals("і північ")) {System.out.println("Ти відкриваєш двері, заходиш у нову схожу кімнату і бачиш бобра.");}
+        if (input.equals("і захід")) {System.out.println("Ти відкриваєш двері, заходиш в нову кімнату і бачиш прямий коридор.");}
+        if (input.equals("і схід")) {System.out.println("Решітка не пускає.");}
+        if (input.equals("в картина")) {System.out.println("\"Ранок у сосновому лісі\" Іван Шишкін та Костянтин Савицький.\n За кртиною був захований ключ і вдалося його підібрати.");}
+        if (input.equals("і північ") == false & input.equals("в картина") == false & input.equals("і схід") == false & input.equals("в картина") == false) {System.out.println("Ти щось не то вводиш.");}
         scan.close();
-        WriteInputToFile(input);
+        String myString = name + "\n" + input;
+        WriteInputToFile(myString);
         ReadInputFromFile();
     }
 }
